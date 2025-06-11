a709a559ba3842b6812b282c1648ad0885325b70
oleg <a112-03@yandex.ru>
2025-06-11 02:15:54 -0700

    v5-9-7

--- 05/terraform-05/providers.tf
+++ 05/terraform-05/providers.tf
@@ -2,6 +2,11 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "> 0.120.0"
+    }
+        template = {
+      source = "hashicorp/template"
+      version = "~> 2.2.0"
     }
   }
   required_version = ">1.8.4"
